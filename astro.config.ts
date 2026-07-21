95d7892f2e7a649a880d13aa94822482eac98b9d
Alex_Xushjie <alexshengchieh@gmail.com>
2026-07-22 00:15:14 +0800

    Initialize Alex's Life with Astro Sify

---
+++ astro.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import { unified } from '@astrojs/markdown-remark';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  site: 'http://astro-sify-demo.lazy-boy-acmer.cn',
+  integrations: [mdx()],
+  vite: {
+    plugins: [
+      tsconfigPaths(),
+    ],
+  },
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      wrap: true,
+    },
+  },
+});
